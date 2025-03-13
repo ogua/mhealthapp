2ec7f1c1abb046cd28e0d452a14385700c556830
Ahmed Ogua <ogua.ahmed18@gmail.com>
2025-03-13 12:46:18 +0000

    updated app

--- components/Constants.tsx
+++ components/Constants.tsx
@@ -1,5 +1,5 @@
 
-export const url = "http://192.168.43.43/api";
+export const url = "http://10.200.6.19:8081/api";
 export const siteurl = "http://192.168.43.43/";
 export const LOCATION_TASK_NAME = 'background-location-task';
 export const BACKGROUND_FETCH_TASK = 'background-FETCH-task';
