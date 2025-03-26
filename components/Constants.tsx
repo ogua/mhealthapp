52761374cdde0d1ac8bfd55a8bab342d1100476f
Ahmed Ogua <ogua.ahmed18@gmail.com>
2025-03-26 14:14:48 +0000

    constant

--- components/Constants.tsx
+++ components/Constants.tsx
@@ -1,6 +1,6 @@
 
-export const url = "http://10.200.6.19:8081/api";
-export const siteurl = "http://192.168.43.43/";
+export const url = "http://192.168.78.30/api";
+export const siteurl = "http://192.168.78.30/";
 export const LOCATION_TASK_NAME = 'background-location-task';
 export const BACKGROUND_FETCH_TASK = 'background-FETCH-task';
 export const tokenname = 'usertoken';
